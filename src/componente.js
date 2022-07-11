3de3b43c35eed584c6d30a81f36f58e8a344f086
Maiten Cullen <maitencullen@gmail.com>
2022-07-11 16:51:05 -0300

    inicialReactcommit

---
+++ src/componente.js
@@ -0,0 +1,16 @@
+
+import ImgIco from './img/01.jpg';
+function Componentes() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <div>
+        <p> hola gente</p>
+        <img src={ImgIco}/>
+       </div>
+      </header>
+    </div>
+  );
+}
+
+export default Componentes;
